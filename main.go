95e889f1e8659f11141300099c6f8b11d9948977
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 12:19:25 -0300

    refact: reducing lines of code

--- main.go
+++ main.go
@@ -9,6 +9,5 @@ func main(){
 var  password string;
   fmt.Printf("You password: ");
   fmt.Scanf("%s",&password);
-  generatePasswordEncrypted :=  encrypting.Encrypt(password,10);
-  fmt.Println("Strong Password: ", generatePasswordEncrypted);
+  encrypting.Encrypt(password,10);
 }
