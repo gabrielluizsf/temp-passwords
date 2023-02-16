c80e0d7aabfa682cc9094bd6402372ef002a9c96
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-02-16 12:15:34 -0300

    main.go defined

--- main.go
+++ main.go
@@ -1,1 +1,14 @@
+package main
 
+import (
+  "github.com/theGOURL/encrypting"
+  "fmt"
+)
+
+func main(){
+var  password string;
+  fmt.Printf("You password: ");
+  fmt.Scanf("%s",&password);
+  generatePasswordEncrypted :=  encrypting.Encrypt(password,10);
+  fmt.Println("Strong Password: ", generatePasswordEncrypted);
+}
